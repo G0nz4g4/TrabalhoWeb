75351dc4ce3ba7d2bf77a9d54331f64a90236415
Ediclei Gonzaga <gonzaga98ed@gmail.com>
2023-06-01 15:44:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function esqueceu() {
+    alert("Sua senha é" + "\n" + "Login: Admin" + "\n" + "Senha: adm123" );
+}
+
+function validar() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario === "admin" && senha === "adm123") {
+        alert("Login realizado com sucesso!")
+
+        window.location.href = "../Projeto/home.html";
+    } else {
+        alert("Login ou Senha estão incorretos!")
+    }
+}
+
+function contato(){
+    window.location.href = "https://wa.me/55081997164112";
+}
